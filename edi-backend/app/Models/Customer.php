5be77a1505ef2d16484dbc92bc6c7996506bbbf0
telagamuhamad <telagah@thebigrichgroup.com>
2024-03-26 02:50:28 +0700

    update: customer model

--- edi-backend/app/Models/Customer.php
+++ edi-backend/app/Models/Customer.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Customer extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity, SoftDeletes;
+
+    protected $table = 'customers';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone_number',
+        'address',
+    ];
+
+    /**
+     * setup activity logs
+     */
+    public function getActivityLogOptions(): LogOptions
+    {
+        return LogOptions::defaults();
+    }
 }
