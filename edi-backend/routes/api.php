2df64cc4c6eab6247343bf40bb46f992d7a26662
telagamuhamad <telagamuhamad@gmail.com>
2024-04-30 15:55:09 +0700

    update

--- edi-backend/routes/api.php
+++ edi-backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\CarController;
+use App\Http\Controllers\DeliveryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('car')->name('car.')->group(function () {
+    Route::get('get-car', [CarController::class, 'getCar'])->name('get-car');
+    Route::post('update-car-status', [CarController::class, 'updateCarStatus'])->name('update-car-status');
+});
+
+Route::prefix('edi')->name('edi.')->group(function () {
+    Route::get('get-travel-document', [DeliveryController::class, 'getData'])->name('get-travel-document');
+    Route::post('update-travel-document', [DeliveryController::class, 'updateStatus'])->name('update-travel-document');
+    Route::get('get-item', [DeliveryController::class, 'getItems'])->name('get-item');
+    Route::post('update-item-status', [DeliveryController::class, 'updateItemStatus'])->name('update-item-status');
 });
